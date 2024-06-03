771266efc05291bd2eb562883f364e9e0872e034
DanilShangin <shangin67886788@gmail.com>
2024-06-03 22:49:19 +0300

    slider news beta 1.0

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ const spiderNews = new Splide('#splideNews',{
     perPage: 2,
     perMove: 1,
     height: '400px',
-    width: '636px',
+    width: '656px',
     gap: '24px',
     autoplay: true,
     arrows: boolean = false,
-    //padding: {left:'10px', rigth:'10px'},
+    padding: {left:'10px', rigth:'10px'},
 });
 
 spiderNews.mount();
