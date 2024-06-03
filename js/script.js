def7a475a56496f2337ef45ff814850618a6107f
DanilShangin <shangin67886788@gmail.com>
2024-06-03 17:25:29 +0300

    splite new

--- js/script.js
+++ js/script.js
@@ -1,10 +1,2 @@
-const swiperNew = new Swiper('.swiper_news', {
-    slidesPerView: 1,
-    speed: 400,
-    spaceBetween: 10,
-    
-    pagination: {
-        el: '.swiper-pagination_news',
-        clickable: true,
-    },
-});
+const swiperNews = new Splite('#spliteNews');
+swiperNews.mount();
