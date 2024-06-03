9ebb222c61e19806b8cfad67fba73aeb44fce315
DanilShangin <shangin67886788@gmail.com>
2024-06-03 16:23:00 +0300

    splite xz

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 const swiperNew = new Swiper('.swiper_news', {
-    
-})
+    slidesPerView: 2,
+    spaceBetween: 30,
+    autoplay: {
+        delay: 5000,
+    },
+    pagination: {
+        el: '.swiper-pagination_news',
+        clickable: true,
+    },
+});
