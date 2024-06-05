f5d42835383ec87d696ec0ce9cb86804b7a2d632
DanilShangin <shangin67886788@gmail.com>
2024-06-05 11:35:08 +0300

    slide event

--- js/event.js
+++ js/event.js
@@ -1,35 +1,33 @@
-const splideEvent = new Splide('#main-carousel', {
+var splide = new Splide('#main-carousel',{
+    type: 'loop',
     width: 856,
-    height: 400,
+    height: 464,
     pagination: false,
-    cover: true,
 });
 
-var thumbnails = new document.getElementsByClassName('thumbnail');
+var thumbnails = document.getElementsByClassName('thumbnail');
 
 for ( var i = 0; i < thumbnails.length; i++ ) {
-    initThumbnail( thumbnails[ i ], i );
+    initThumbnail(thumbnails[ i ], i );
   }
 
 function initThumbnail( thumbnail, index ) {
     thumbnail.addEventListener( 'click', function () {
-        splideEvent.go(index);
+        splide.go(index);
     } );
 }
 
-splideEvent.on( 'mounted move', function () {
-    var thumbnail = thumbnails[ splide.index ];
+// splideEvent.on( 'mounted move', function () {
+//     var thumbnail = thumbnails[ splide.index ];
   
-    if ( thumbnail ) {
-      if ( current ) {
-        current.classList.remove( 'is-active' );
-      }
+//     if ( thumbnail ) {
+//       if ( current ) {
+//         current.classList.remove( 'is-active' );
+//       }
   
-      thumbnail.classList.add( 'is-active' );
-      current = thumbnail;
-    }
-  } );
-  
-  splide.mount();
+//       thumbnail.classList.add( 'is-active' );
+//       current = thumbnail;
+//     }
+//   } );
 
-splideEvent.mount();
+splide.mount();
