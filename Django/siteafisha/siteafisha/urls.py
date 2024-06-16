83ab86c930c7c09815b37a817d7fc3423b0add65
DanilShangin <shangin67886788@gmail.com>
2024-06-16 13:40:01 +0300

    urls

--- Django/siteafisha/siteafisha/urls.py
+++ Django/siteafisha/siteafisha/urls.py
@@ -15,10 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
-from afiha.views import index
+from django.urls import path, include
+from afiha import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', index),
+    path('', include('afiha.urls')),
 ]
