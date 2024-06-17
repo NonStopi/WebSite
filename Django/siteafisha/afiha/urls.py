0ab0e5d66c3965da5a8a8ab95df794ab74201b3a
DanilShangin <shangin67886788@gmail.com>
2024-06-17 22:53:36 +0300

    date

--- Django/siteafisha/afiha/urls.py
+++ Django/siteafisha/afiha/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.index),
     path('contact/', views.contact),
+    path('poster/<int:poster_id>/', views.poster),
+    path('poster/<slug:poster_name>/', views.poster_name),
+    re_path(r"^poster/(?P<date>\d{2}/\d{2}/\d{4})/$", views.poster_date)
 ]
--- Django/siteafisha/afiha/views.py
+++ Django/siteafisha/afiha/views.py
@@ -7,3 +7,12 @@ def index(request):
 
 def contact(request):
     return HttpResponse("<h1>контакты</h1>")
+
+def poster(request, poster_id):
+    return HttpResponse(f"<h1>постер №: {poster_id}</h1>")
+
+def poster_name(request, poster_name):
+    return HttpResponse(f"<h1>постер name: {poster_name}</h1>")
+
+def poster_date(request, date):
+    return HttpResponse(f"<h1>архив: {date}</h1>")
